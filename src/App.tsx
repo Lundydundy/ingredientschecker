74a9987f3d8190a753a7e984cf55dad592dd54de
Matteo Giliotti <matteogiliotti@gmail.com>
2024-05-03 12:07:00 -0400

    allergy names

--- src/App.tsx
+++ src/App.tsx
@@ -82,9 +82,9 @@ function App() {
       const data = await response.json();
       console.log(data.result);
 
-      setAllergy(data.result.found)
+      await setAllergy(data.result.words)
       
-      let cvimg = await cv.imread(imgElement);
+      let cvimg = cv.imread(imgElement);
 
       for await (const box of data.result.boxes) {
         const x0 = box.x0, y0 = box.y0, x1 = box.x1, y1 = box.y1;
