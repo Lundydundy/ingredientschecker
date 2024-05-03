db2796d35cf4db0e455fe513a10e26d035f1c7b9
Matteo Giliotti <matteogiliotti@gmail.com>
2024-05-03 12:01:30 -0400

    image change

--- api/server.js
+++ api/server.js
@@ -101,8 +101,9 @@ app.post("/", (req, res) => {
 app.post("/processimg", upload.single('file'), async (req, res) => {
 
     const worker = await createWorker('eng');
+    
 
-    const result = await worker.recognize(`${req.file.originalname}`, {
+    const result = await worker.recognize(`./images/${req.file.originalname}`, {
         tessedit_char_blacklist: '0123456789!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~', // Ignore numbers and special characters
         oem: 1
     })
